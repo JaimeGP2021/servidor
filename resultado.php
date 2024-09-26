db802b30cf306fb51586dad7a58091a9635d9e1d
Jaime García <jaime.garcia@iesdonana.org>
2024-09-26 21:18:16 +0200

    Calculadora disfuncional

--- resultado.php
+++ resultado.php
@@ -8,7 +8,27 @@
 </head>
 
 <body>
-    <p>El resultado es <?= $_GET['op1'] + $_GET['op2'] ?></p>
+    <p>
+        <?php
+        if (isset($_GET['op1']) && !empty($_GET['op1']) && is_numeric($_GET['op1'])) {
+            $op1 = trim($_GET['op1']);
+        } else {
+            echo "<h3>EL primer operando no es correcto</h3>";
+        }
+
+        if (isset($_GET['op2']) && !empty($_GET['op2']) && is_numeric($_GET['op2'])) {
+            $op2 = trim($_GET['op2']);
+        } else {
+            echo "<h3>EL segundo operando no es correcto</h3>";
+        }
+
+        if (isset($op1, $op2)){
+            $res = $op1 + $op2;
+            $mensaje = "La suma de $op1 y $op2 es $res.";
+        }
+        ?>
+        <?= $mensaje ?>
+    </p>
 </body>
 
 </html>
