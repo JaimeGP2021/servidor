084e9c10be3d242c38e54da7ff0aa60ab9736077
Jaime García <jaime.garcia@iesdonana.org>
2024-09-26 22:26:12 +0200

    Calculadora funcional

--- resultado.php
+++ resultado.php
@@ -13,18 +13,48 @@
         if (isset($_GET['op1']) && !empty($_GET['op1']) && is_numeric($_GET['op1'])) {
             $op1 = trim($_GET['op1']);
         } else {
-            echo "<h3>EL primer operando no es correcto</h3>";
+            echo "<h3>El primer operando no es correcto</h3>";
         }
 
         if (isset($_GET['op2']) && !empty($_GET['op2']) && is_numeric($_GET['op2'])) {
             $op2 = trim($_GET['op2']);
         } else {
-            echo "<h3>EL segundo operando no es correcto</h3>";
+            echo "<h3>El segundo operando no es correcto</h3>";
         }
 
-        if (isset($op1, $op2)){
-            $res = $op1 + $op2;
-            $mensaje = "La suma de $op1 y $op2 es $res.";
+        $operadores = ['+', '-', '*', '/'];
+        if (isset($_GET['op']) && !empty($_GET['op']) && in_array($_GET['op'], $operadores)) {
+            $op = trim($_GET['op']);
+        } else {
+            echo "<h3>El operador no es correcto</h3>";
+        }
+
+        if (isset($op1, $op2, $op)) {
+            switch ($op) {
+                case '+':
+                    $res = $op1 + $op2;
+                    $mensaje = "La suma de $op1 y $op2 es $res.";
+                    break;
+
+                case '-':
+                    $res = $op1 - $op2;
+                    $mensaje = "La resta de $op1 y $op2 es $res.";
+                    break;
+
+                case '*':
+                    $res = $op1 + $op2;
+                    $mensaje = "La multiplicación de $op1 y $op2 es $res.";
+                    break;
+
+                case '/':
+                    $res = $op1 / $op2;
+                    $mensaje = "La división de $op1 entre $op2 es $res.";
+                    break;
+
+                default:
+                    $mensaje = "Algo salió mal";
+                    break;
+            }
         }
         ?>
         <?= $mensaje ?>
